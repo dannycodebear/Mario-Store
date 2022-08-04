4c11aa89db00c2934eeffbd1b63f39de1b70bc4f
Danny Kao <denny00912@gmail.com>
2022-08-04 18:21:14 +0800

    add passport.js and item-route.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 const app = express();
 import mongoose from "mongoose";
-import fetch from "node-fetch";
 import dotenv from "dotenv";
 dotenv.config();
 import authRouter from "./routes/auth.js";
+import itemRouter from "./routes/item-route.js";
+import { pst } from "./config/passport.js";
+import passport from "passport";
+pst(passport);
 
 mongoose
   .connect(process.env.DB_CONNECT)
@@ -19,6 +22,7 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/user", authRouter);
+app.use("/api/items", passport.authenticate("jwt", { session: false }), itemRouter);
 
 app.listen(8080, () => {
   console.log("Server is running on the port 8080");
