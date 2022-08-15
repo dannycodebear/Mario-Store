12f17b2ff5abc8d12f0cebeb64b09a60bce60225
Danny Kao <denny00912@gmail.com>
2022-08-15 20:46:32 +0800

    add props

--- src/components/login-component.js
+++ src/components/login-component.js
@@ -2,54 +2,39 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import AuthService from "../service/auth-service.js";
 
-const LoginComponent = () => {
+const LoginComponent = (props) => {
+  //用 props 接過來的要用中括號
+  const { display, setDisplay } = props;
   const navigate = useNavigate();
-  const [username, setUsername] = useState("");
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [role, setRole] = useState("");
 
-  const handleChangeUsername = (e) => {
-    setUsername(e.target.value);
-  };
   const handleChangeEmail = (e) => {
     setEmail(e.target.value);
   };
   const handleChangePassword = (e) => {
     setPassword(e.target.value);
   };
-  const handleChangeRole = (e) => {
-    setRole(e.target.value);
+
+  const handleReset = () => {
+    return setEmail(""), setPassword("");
   };
 
-  const handleRegister = () => {
-    AuthService.register(username, email, password, role)
-      .then(() => {
-        window.alert("Register Successfully !!!");
-        navigate("/");
-      })
-      .catch((e) => {
-        console.log(e.response);
-      });
+  const handleDisplay = () => {
+    setDisplay(2);
+    navigate("/register");
   };
-  const handleReset = () => {
-    return setUsername(""), setEmail(""), setPassword(""), setRole("");
-  };
+
   return (
     <div className="register-form">
       <div className="changeMode">
         <button>登入頁面</button>
-        <button>註冊頁面</button>
+        <button onClick={handleDisplay}>
+          <p>註冊頁面</p>
+        </button>
       </div>
       <div className="divInput">
-        <p>Username:</p>
-        <input
-          onChange={handleChangeUsername}
-          type="text"
-          name="username"
-          value={username}
-          required
-        />
         <p>Email:</p>
         <input onChange={handleChangeEmail} type="email" name="email" value={email} required />
         <p>Password:</p>
@@ -60,20 +45,11 @@ const LoginComponent = () => {
           value={password}
           required
         />
-        <p>Role:</p>
-        <label for="role">
-          <select onChange={handleChangeRole} name="role" id="role">
-            <option value={role}></option>
-            <option value="admin">Admin</option>
-            <option value="member">Member</option>
-          </select>
-        </label>
       </div>
 
       <div className="divButton">
-        <button onClick={handleRegister} type="submit">
-          Register
-        </button>
+        {display === 0 && display === 1 && <button type="submit">Login</button>}
+        <button type="submit">Login</button>
         <button onClick={handleReset} type="reset">
           Reset
         </button>
