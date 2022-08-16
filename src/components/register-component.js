2e914242a6b6cd0e04a731ac3d5ae4afc9140c5f
Danny Kao <denny00912@gmail.com>
2022-08-16 17:01:33 +0800

    setting useEffect to handle display error

--- src/components/register-component.js
+++ src/components/register-component.js
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 import AuthService from "../service/auth-service.js";
 
 const RegisterComponent = (props) => {
+  useEffect(() => {
+    setDisplay(2);
+  }, []);
+
   //用 props 接過來的要用中括號
   const { display, setDisplay } = props;
   const navigate = useNavigate();
@@ -10,6 +14,7 @@ const RegisterComponent = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [role, setRole] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleChangeUsername = (e) => {
     setUsername(e.target.value);
@@ -30,12 +35,15 @@ const RegisterComponent = (props) => {
         window.alert("Register Successfully !!!");
         navigate("/login");
       })
-      .catch((e) => {
-        console.log(e.response);
+      .catch((error) => {
+        console.log(error.response);
+        // 顯示 error message 給用戶
+        setErrorMessage(error.response.data);
       });
   };
+
   const handleReset = () => {
-    return setUsername(""), setEmail(""), setPassword(""), setRole("");
+    return setUsername(""), setEmail(""), setPassword(""), setRole(""), setErrorMessage("");
   };
 
   const handleDisplay = () => {
@@ -51,6 +59,8 @@ const RegisterComponent = (props) => {
         </button>
         <button>註冊頁面</button>
       </div>
+      {/* 若 error message 是 false = 沒有  就不會顯示 */}
+      {errorMessage && <div className="alert-message">{errorMessage}</div>}
       <div className="divInput">
         <p>Username:</p>
         <input
@@ -74,8 +84,8 @@ const RegisterComponent = (props) => {
         <label for="role">
           <select onChange={handleChangeRole} name="role" id="role" value={role} required>
             <option value=""></option>
-            <option value="Member">Member</option>
-            <option value="Admin">Admin</option>
+            <option value="member">member</option>
+            <option value="admin">admin</option>
           </select>
         </label>
       </div>
